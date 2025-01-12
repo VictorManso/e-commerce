fa46e84aac557a7b820b492dbd4b6b03b989f161
Victor Morais <victor.manso@outlook.com.br>
2025-01-12 17:05:34 -0300

    animação do banner inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useLayoutEffect } from "react";
 import Example from "./components/menu";
 import { TypeAnimation } from "react-type-animation";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to("#tenis", {
+      duration: 3,
+      x: 0,
+      opacity: 1,
+    });
+
+    return () => {
+      gsap.killTweensOf("#tenis");
+    };
+  }, []);
+
   return (
     <div>
       <Example />
-      <div className="text-1xl text-slate-500 bold font-bold p-4">
+      <div className="text-1xl text-slate-500 bold font-bold p-4 text-center">
         <TypeAnimation
           sequence={[
             "Seu e-commerce de confiança. Encontre o que você precisa aqui.",
@@ -19,6 +35,25 @@ function App() {
           speed={70}
           repeat={Infinity}
         />
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center">
+        <div className="m-8">
+          <img
+            className=" transform: -translate-x-full opacity-0 w-50 border-solid border-2 border-gray-500 rounded-3xl shadow-2xl"
+            src="./src/assets/images/tenis.png"
+            id="tenis"
+          />
+        </div>
+
+        <div className="m-8">
+          <h2 className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            officiis ab dolorem vitae sit esse dolor ipsum voluptatem quae,
+            dignissimos accusantium molestias et dicta distinctio nobis nulla
+            suscipit placeat doloribus.
+          </h2>
+        </div>
       </div>
     </div>
   );
