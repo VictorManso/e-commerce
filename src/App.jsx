36325306f8b07f119abb14b20b0820bdf7ba1d5f
Victor Morais <victor.manso@outlook.com.br>
2025-01-12 23:44:56 -0300

    estilização sombras

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,58 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
+import { gsap } from "gsap";
 import Example from "./components/menu";
 import { TypeAnimation } from "react-type-animation";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { CalculatorIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 function App() {
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to("#tenis", {
-      duration: 3,
-      x: 0,
-      opacity: 1,
-    });
+  useEffect(() => {
+    // Defina uma função para disparar quando a página estiver completamente carregada
+    const handleWindowLoad = () => {
+      gsap.registerPlugin();
 
-    return () => {
-      gsap.killTweensOf("#tenis");
+      gsap.to("#tenis", {
+        duration: 2,
+        x: 0,
+        opacity: 1,
+        filter: "blur(0px)",
+        filter: "drop-shadow(2px 4px 6px)",
+      });
+
+      gsap.to("#tennis-text", {
+        duration: 2,
+        x: 0,
+        opacity: 1,
+        filter: "blur(0px)",
+      });
+
+      gsap.to("#button-banner", {
+        duration: 2,
+        x: 0,
+        opacity: 1,
+        filter: "blur(0px)",
+      });
     };
-  }, []);
 
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to("#texto-tenis", {
-      duration: 3,
-      x: 0,
-      opacity: 1,
-    });
+    // Adiciona o evento de load ao window
+    window.onload = handleWindowLoad;
 
     return () => {
-      gsap.killTweensOf("#texto-tenis");
+      // Limpa o evento quando o componente é desmontado
+      window.onload = null;
     };
   }, []);
 
   return (
-    <div className="bg-slate-700">
+    <div className="bg-slate-300">
       <Example />
-      <div className=" min-h-40 text-2xl text-slate-200 bold font-bold p-4 text-center content-center">
+      <div className="min-h-40 text-2xl text-slate-600 bold font-bold p-4 text-center content-center">
         <TypeAnimation
           sequence={[
-            "Seu e-commerce de confiança. Encontre o que você precisa aqui.",
+            "Your trusted e-commerce. Find what you need here.",
             2000,
-            "Frete grátis para todo o Brasil! Não perca essa oportunidade.",
+            "Free shipping to all of Brazil! Don't miss this opportunity.",
             2000,
-            "Ofertas imperdíveis! Aproveite descontos incríveis por tempo limitado.",
+            "Unmissable offers! Enjoy incredible discounts for a limited time.",
             2000,
           ]}
           wrapper="span"
@@ -51,23 +62,31 @@ function App() {
       </div>
 
       <div className="flex flex-col sm:flex-row items-center">
-        <div className="m-8  place-items-center ">
+        <div className="m-8 place-items-center relative">
           <img
-            className="w-full transform: -translate-x-full opacity-0 rounded-3xl shadow-2xl"
+            className="w-lvw opacity-0 rounded-3xl -translate-x-full drop-shadow-2xl"
             src="./src/assets/images/tenis.png"
             id="tenis"
           />
         </div>
 
-        <div className="m-8 w-full">
+        <div className="m-8 w-full text-center place-items-center">
           <h2
-            className="transform: translate-x-full font-bold text-4xl text-center align-middle text-slate-200"
-            id="texto-tenis"
+            className="font-bold text-4xl text-center align-middle text-slate-600 opacity-0 translate-x-full blur-md"
+            id="tennis-text"
           >
-            Tênis Air Jordan 1 High Chicago <br /> "Lost and Found"
+            Air Jordan 1 High Chicago Sneakers <br /> "Lost and Found"
           </h2>
+          <button
+            className="shadow-custom flex m-20 font-bold text-slate-600 bg-slate-300 items-center p-4 rounded-lg text-2xl text-center opacity-0 translate-x-full blur-md"
+            id="button-banner"
+          >
+            <ShoppingCartIcon className="w-8 h-8 mr-4" /> Add to Cart
+          </button>
         </div>
       </div>
+
+      <div className="text-3xl">carrossel</div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0px 7px 32px -7px rgb(149 163 184 / 2.25)",
+      },
+    },
   },
   plugins: [],
 };
