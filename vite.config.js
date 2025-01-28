418eb571307324620169bf709126cae3a798774d
Victor Morais <victor.manso@outlook.com.br>
2025-01-28 20:34:19 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/e-commerce/",
   server: {
-    host: true, // Permite conexões externas
-    port: 3000, // Porta para o servidor (pode ajustar)
+    host: true, 
+    port: 3000, 
   },
 });
